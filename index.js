c48a18303bc4ae52d49b9a701cb2bdd33438769b
ItzmeJan <87923447+ItzmeJan@users.noreply.github.com>
2025-11-23 01:06:44 -0500

    updated parseTokens

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const data = fs.readFileSync("./input.txt").toString();
 
 function parseTokens() {
-    const tokens = data.split(/\s+/)
+    const tokens = data.trim().split(/[^\p{L}\p{N}]+/u).filter(Boolean);
     return tokens;
 }
 
